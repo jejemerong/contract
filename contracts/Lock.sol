fc1f1e640f3a5a60e8177eb3ca60824a4385cc13
jejemerong <9708jjw@gmail.com>
2023-07-10 13:50:11 +0900

    Init: fix config to typescript

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,11 +1,14 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
 
 contract Lock {
     uint public unlockTime;
     address payable public owner;
 
-    event Withdrawal(uint amount, uint when); // 언락 후 출금 이벤트 기록
+    event Withdrawal(uint amount, uint when);
 
     constructor(uint _unlockTime) payable {
         require(
@@ -17,13 +20,14 @@ contract Lock {
         owner = payable(msg.sender);
     }
 
-    // 출금
     function withdraw() public {
+        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
+        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
 
         require(block.timestamp >= unlockTime, "You can't withdraw yet");
         require(msg.sender == owner, "You aren't the owner");
 
-        emit Withdrawal(address(this).balance, block.timestamp); // address(this) 해당 주소 객체
+        emit Withdrawal(address(this).balance, block.timestamp);
 
         owner.transfer(address(this).balance);
     }
