1dc20b478d12332e01938fd22bfdfa0282a90f99
jejemerong <9708jjw@gmail.com>
2023-07-19 16:48:02 +0900

    Fix: previous config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,18 +1,6 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require('@nomicfoundation/hardhat-toolbox')
 
-const { API_URL, PRIVATE_KEY } = process.env;
-
-const config: HardhatUserConfig = {
-  solidity: "0.8.20",
-  defaultNetwork: "goerli",
-  networks: {
-    hardhat: {},
-    goerli: {
-      url: API_URL,
-      accounts: [`0x${PRIVATE_KEY}`],
-    },
-  },
-};
-
-export default config;
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: '0.8.19',
+}
