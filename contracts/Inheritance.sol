4b4bb8a8f52cf6ab296fe12fd542bb75a7ee959e
jejemerong <9708jjw@gmail.com>
2023-07-22 21:10:40 +0900

    Add: inheritance

---
+++ contracts/Inheritance.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract A {
+    function foo () public pure virtual returns(string memory) {
+        return "A";
+    }
+
+    function bar() public pure virtual returns(string memory){
+        return "A";
+    }
+}
+
+contract B is A {
+    // 상속 받은 부모 컨트랙트 내의 함수를 사용하기 위해서는 override 명시 필요
+    function foo() public pure override virtual returns (string memory){
+        return "B";
+    }
+
+    function bar () public pure override virtual returns(string memory) {
+        return "B";
+    }
+}
+
+// 아니 근데 B 한테만 상속 받아도 되는 거 아닌가?
+// 왜 굳이 A, B 로 상속 받아야 하는 거지?
+contract C is A, B {
+    function foo() public pure override(A, B) returns(string memory) {
+        return "C";
+    }
+
+    function bar() public pure override(A, B) returns(string memory){
+        return "C";
+    }
+}
+
